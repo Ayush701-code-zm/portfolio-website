84193d7479cfb89edf7a431c0a6612926025075e
Ayush701-code-zm <iamayushanand365@gmail.com>
2024-09-21 16:51:39 +0530

    changes

---
+++ src/Details.js
@@ -0,0 +1,178 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/fotor-202409210244.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import node from "./assets/techstack/node1.png";
+// import redux from "./assets/techstack/redux.png";
+// import tailwind from "./assets/techstack/tailwind.png";
+import express from "./assets/techstack/express-js.png";
+import mongodb from "./assets/techstack/image.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Ayush Anand",
+  tagline: "I build things for web",
+  img: profile,
+  about: `My name is Ayush. I am a passionate and driven programmer with a strong enthusiasm for learning and growth. With a self-taught mindset, I continuously explore new technologies and sharpen my problem-solving skills. I am particularly drawn to all aspects of web application development, where I focus on making the web more accessible and engaging. My core expertise lies in JavaScript, and I especially enjoy working with React to build dynamic, user-friendly interfaces. I am open to job opportunities that align with my skills and interests, and I am always eager to take on new challenges.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/ayush-anand-9235b7230/",
+  github: "https://github.com/Ayush701-code-zm",
+  twitter: "https://x.com/ayush_zm",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  // {
+  //   Position: "Frontend Web Developer",
+  //   Company: `Company Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Full Time",
+  //   Duration: "Sep 2021 - Dec 2021",
+  // },
+  {
+    Position: "FullStack Web Developer",
+    Company: `Farmershop Tech India Pvt. Ltd.`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Jan 2024 - Sep 2024",
+  },
+  {
+    Position: "Frontend Web Developer",
+    Company: `Technerd Academy`,
+    Location: "Chandigarh",
+    Type: "FreeLancing",
+    Duration: "Aug 2024 - Sep 2024",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Bachelor in Electronics & Communication",
+    Company: `Your College Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  node: node,
+  // redux: redux,
+  // sass: sass,
+  // tailwind: tailwind,
+  express: express,
+  mongodb: mongodb,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Zomato Landing Page",
+    image: projectImage1,
+    description: `Discover a vibrant landing page for Zomato that highlights local dining options and food delivery services. Easily search for restaurants, explore diverse cuisines, and enjoy user-friendly navigation. Perfect for food lovers looking to dine out or order in!`,
+    techstack: "HTML,CSS",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/Ayush701-code-zm/zomato-landing-page-zm",
+  },
+  {
+    title: "Task Management Project",
+    image: projectImage2,
+    description: `A streamlined task management application designed to help users organize their daily activities. Features include task creation, deadlines, priority levels, and progress tracking. Ideal for individuals and teams seeking to enhance productivity and collaboration.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/Ayush701-code-zm/task-management-project",
+  },
+  {
+    title: "Admin Dashboard",
+    image: projectImage3,
+    description: `A responsive admin dashboard built with React, providing real-time insights and data visualization. Features include user management, analytics charts, customizable widgets, and seamless navigation. Perfect for managing applications and monitoring performance efficiently.`,
+    techstack: "CSS, JavaScript, React",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/Ayush701-code-zm/react-admin-dashboard",
+  },
+//   {
+//     title: "Project title 4",
+//     image: projectImage4,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 5",
+//     image: projectImage5,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 6",
+//     image: projectImage6,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "iamayushanand365@gmail.com",
+  phone: "+91 9693329842",
+};
---
+++ src/Pages/Technologies.js
@@ -0,0 +1,259 @@
+import React from "react";
+import { techStackDetails } from "../Details";
+
+function Technologies() {
+  const {
+    html,
+    css,
+    js,
+    react,
+    node,
+    // redux,
+    express,
+    bootstrap,
+    mongodb,
+    sass,
+    vscode,
+    git,
+    github,
+    npm,
+    postman,
+    figma,
+  } = techStackDetails;
+
+  return (
+    <main
+      className="container mx-auto max-width pt-10 pb-20"
+      style={{
+        /* Add global styles here */
+      }}
+    >
+      <section>
+        <h1
+          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+          style={{
+            /* Add header styles here */
+          }}
+        >
+          Tech Stack
+        </h1>
+        <p
+          className="text-content py-2 lg:max-w-3xl"
+          style={{
+            /* Add paragraph styles here */
+          }}
+        >
+          Technologies I've been working with recently
+        </p>
+      </section>
+      <section
+        className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6"
+        style={{
+          /* Add grid styles here */
+        }}
+      >
+        <img
+          src={html}
+          title="html"
+          alt=""
+          className="image-icon"
+          style={{
+            /* Add image styles here */
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={css}
+          title="CSS"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={js}
+          title="JavaScript"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={react}
+          title="React"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={node}
+          title="Node"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={express}
+          title="Express"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        {/* <img src={mongodb} title="MongoDB" alt="" className="image-icon" style={{ width: "50px", height: "50px", objectFit: "contain", margin: "10px" }} /> */}
+        <img
+          src={bootstrap}
+          title="Bootstrap"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={mongodb}
+          title="MongoDB"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        {/* <img
+          src={sass}
+          title="SASS"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        /> */}
+      </section>
+      <section>
+        <h1
+          className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+          style={{
+            /* Add header styles here */
+          }}
+        >
+          Tools
+        </h1>
+      </section>
+      <section
+        className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6"
+        style={{
+          /* Add grid styles here */
+        }}
+      >
+        <img
+          src={vscode}
+          title="Visual Studio Code"
+          alt=""
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={git}
+          title="Git"
+          alt="Git"
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={github}
+          title="Github"
+          alt="Github"
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={figma}
+          title="Figma"
+          alt="Figma"
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={npm}
+          title="NPM"
+          alt="NPM"
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+        <img
+          src={postman}
+          title="Postman"
+          alt="Postman"
+          className="image-icon"
+          style={{
+            width: "50px",
+            height: "50px",
+            objectFit: "contain",
+            margin: "10px",
+          }}
+        />
+      </section>
+    </main>
+  );
+}
+
+export default Technologies;
